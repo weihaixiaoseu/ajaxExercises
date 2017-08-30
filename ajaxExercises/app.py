cf5186ad2931e6d3e9ab458c88de957d10dbc8eb
Your Name <you@example.com>
2017-08-30 19:03:31 +0800

    flask ajax get

---
+++ ajaxExercises/app.py
@@ -0,0 +1,24 @@
+from flask import Flask, url_for, render_template, jsonify
+from flask_bootstrap import Bootstrap
+from random import randint 
+
+
+
+app = Flask(__name__)
+bootstrap = Bootstrap(app)
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+
+
+@app.route('/')
+def index():
+	number = randint(1,100)
+	return render_template('index.html', number=number)
+
+@app.route('/rando')
+def rando(): 
+	number = randint(1,100)
+	return jsonify({"no":randint(1,100)})
+
+if __name__=='__main__':
+	app.run(debug=True)
+
